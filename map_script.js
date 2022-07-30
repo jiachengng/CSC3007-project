36ab692f955e37af56c600aace13ca2ab3ae629b
wj-tan <60432453+wj-tan@users.noreply.github.com>
2022-07-30 15:27:35 +0800

    Added icons

--- map_script.js
+++ map_script.js
@@ -2,7 +2,7 @@
 const apiUrl =
   "https://jiachengng.github.io/CSC3007-project/dataset/dengue_cluster.json";
 
-const clinicJson = "";
+const clinicJson = "https://jiachengng.github.io/CSC3007-project/dataset/clinic.json";
 
 // Create Layer Object
 let tiles = new L.tileLayer(
@@ -28,7 +28,7 @@ let map = new L.Map("map", {
 fetch(apiUrl)
   .then((response) => response.json())
   .then((data) => {
-    console.log(data);
+    // console.log(data);
 
     // console.log(myArray)
 
@@ -88,3 +88,92 @@ fetch(apiUrl)
 
     // Drawing Clinc
   });
+
+  fetch(clinicJson)
+  .then((response) => response.json())
+  .then((data) => {
+    // console.log(data.SrchResults[1].LatLng);
+
+    var greenIcon = L.icon({
+        iconUrl: 'leaf-green.png',
+        shadowUrl: 'leaf-shadow.png',
+    
+        iconSize:     [38, 95], // size of the icon
+        shadowSize:   [50, 64], // size of the shadow
+        iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
+        shadowAnchor: [4, 62],  // the same for the shadow
+        popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
+    });
+
+    const myArray2 = [];
+    for (let i = 1; i < data.SrchResults.length; i++) {
+        var example = data.SrchResults[i].LatLng;
+        var tempArray = [];
+        tempArray.push(example)
+        tempArray.forEach((xElement) => {
+            // console.log(xElement)
+            tempArray.push(parseFloat(xElement));
+          });
+          myArray2.push(tempArray);
+        L.marker(myArray2, {icon: greenIcon}).addTo(map);
+     }
+    // console.log(myArray)
+
+    // Drawing Polygons Start
+    // for (let i = 1; i < data.SrchResults.length; i++) {
+    //   var example = data.SrchResults[i].LatLng;
+    //   const myArray = example.split("|");
+    //   var dengueCase = data.SrchResults[i].CASE_SIZE;
+    //   var locationName = data.SrchResults[i].DESCRIPTION;
+
+    //   const myArray2 = [];
+    //   myArray.forEach((element) => {
+    //     // console.log(element)
+    //     var x = element.split(",");
+    //     var tempArray = [];
+    //     x.forEach((xElement) => {
+    //       // console.log(xElement)
+    //       tempArray.push(parseFloat(xElement));
+    //     });
+    //     myArray2.push(tempArray);
+    //   });
+    //   var finalArray = [];
+    //   finalArray.push(myArray2);
+    //   //   console.log(finalArray)
+
+    //   // Creating multi polygon options
+    //   if (dengueCase <= 11) {
+    //     var multiPolygonOptions = { color: "yellow", weight: 8 };
+    //   } else if (dengueCase > 11 && dengueCase < 200) {
+    //     var multiPolygonOptions = { color: "orange", weight: 8 };
+    //   } else {
+    //     var multiPolygonOptions = { color: "red", weight: 8 };
+    //   }
+    //   // Creating multi polygons
+    //   var multipolygon = L.multiPolygon(finalArray, multiPolygonOptions);
+
+    //   // Adding multi polygon to map
+    //   multipolygon.addTo(map);
+
+    //   // create popup contents
+    //   var customPopup =
+    //     "No of Cases: " + dengueCase + "<br>" + "Location: " + locationName;
+    //   // specify popup options
+    //   var customOptions = {
+    //     maxWidth: "400",
+    //     width: "200",
+    //     className: "popupCustom",
+    //   };
+
+    //   multipolygon.bindPopup(customPopup, customOptions);
+    //   //   multipolygon.bindPopup(
+    //   //     "No of Cases: " + dengueCase + "<br>" + "Location: " + locationName
+    //   //   );
+    //   // L.polygon(finalArray).bindTooltip("my tooltip").addTo(map);
+    // }
+    //Drawing Polygon END
+
+    // Drawing Clinc
+  });
+
+
